c3cfafe578a0d4d92fe0fe21c6a5bb5c92dd9c0e
besito <evitabak@gmail.com>
2018-10-28 01:47:30 +0000

    added me statement

--- Week4/Twelve.java
+++ Week4/Twelve.java
@@ -14,12 +14,18 @@ import java.util.function.Function;
 
 //Evita Bakopoulou, UCInet: ebakopou
 
+
 class Twelve{
-    public static HashMap<String, Object> data_storage_obj = new HashMap<String, Object>();
-    public static HashMap<String, Object> stop_words_obj =  new HashMap<String, Object>();
-    public static HashMap<String, Object> word_freq_obj = new HashMap<String, Object>();
     
-    public static void extract_words(HashMap<String, Object> obj, String filePath) throws IOException{
+    static class Me<Key, Value> extends HashMap<Key, Value> {
+        private final Me<Key, Value> me = this;
+    }
+    
+    public static Me<String, Object> data_storage_obj = new Me<String, Object>();
+    public static Me<String, Object> stop_words_obj =  new Me<String, Object>();
+    public static Me<String, Object> word_freq_obj = new Me<String, Object>();
+    
+    public static void extract_words(Me<String, Object> me, String filePath) throws IOException{
         List<String> words = new ArrayList<String>();
 		String line;
         try{
@@ -32,7 +38,7 @@ class Twelve{
         		  words.add(w);
         	     }
         	}
-        	obj.put("data", words);
+        	me.put("data", words);
         }
         catch (IOException e){
         	e.printStackTrace();
@@ -40,7 +46,7 @@ class Twelve{
     }
     
     
-    public static void load_stop_words(HashMap<String, Object> obj) throws IOException{
+    public static void load_stop_words(Me<String, Object> me) throws IOException{
 		String stopwords_path = "../stop_words.txt";
 		String line;
         List<String> stopword_list = new ArrayList<String>();
@@ -56,17 +62,17 @@ class Twelve{
         catch (IOException e){
         	e.printStackTrace();
         }
-        obj.put("stop_words", stopword_list);
+        me.put("stop_words", stopword_list);
     }
 
     
-    public static void increment_count(HashMap<String, Object> freq_obj) {
-		HashMap<String, Integer> freq = ((HashMap<String, Integer>) word_freq_obj.get("freqs"));
-		List<String> words = (ArrayList<String>) data_storage_obj.get("words");
+    public static void increment_count(Me<String, Object> me) {
+		Me<String, Integer> freq = ((Me<String, Integer>) me.get("freqs"));
+		List<String> words = (ArrayList<String>) data_storage_obj.me.get("words");
 		boolean is_stopword;
 		for (String word : words) {
 		    if(word.length()>1){
-    		        is_stopword = ((Function<String, Boolean>) stop_words_obj.get("is_stop_word")).apply(word);
+    		        is_stopword = ((Function<String, Boolean>) stop_words_obj.me.get("is_stop_word")).apply(word);
         		    if(!is_stopword){
             		      if (freq.containsKey(word)){
             				freq.put(word, freq.get(word) + 1);}
@@ -76,48 +82,48 @@ class Twelve{
     			     }
 		     }
 	     }
-		freq_obj.put("freqs", freq);
+		me.put("freqs", freq);
 	}
     
     public static void main(String[] args) throws IOException{
         List<String> data = new ArrayList<>();
-        data_storage_obj.put("data", data);
-        data_storage_obj.put("init", (Runnable) (() -> {
+        data_storage_obj.me.put("data", data);
+        data_storage_obj.me.put("init", (Runnable) (() -> {
             try{
-                extract_words(data_storage_obj, args[0]);
+                extract_words(data_storage_obj.me, args[0]);
             }
             catch(IOException e){
                 e.printStackTrace();
             }
         }));
-        ((Runnable) data_storage_obj.get("init")).run();
+        ((Runnable) data_storage_obj.me.get("init")).run();
         
-        List<String> words = (List<String>)data_storage_obj.get("data");
-        data_storage_obj.put("words", words);
+        List<String> words = (List<String>)data_storage_obj.me.get("data");
+        data_storage_obj.me.put("words", words);
         
-        HashMap<String, Object> stop_words_obj = new HashMap<>();
-        stop_words_obj.put("stop_words", new ArrayList<String>());
-        stop_words_obj.put("init", (Runnable) ( () -> {
+        Me<String, Object> stop_words_obj = new Me<>();
+        stop_words_obj.me.put("stop_words", new ArrayList<String>());
+        stop_words_obj.me.put("init", (Runnable) ( () -> {
             try{
-                load_stop_words(stop_words_obj);
+                load_stop_words(stop_words_obj.me);
             }
             catch(IOException e){
                 e.printStackTrace();
             }
         }));
         
-        stop_words_obj.put("is_stop_word", (Function<String, Boolean>) w -> ((List<String>)stop_words_obj.get("stop_words")).contains(w));
-        words = (List<String>)data_storage_obj.get("words");
-        ((Runnable) stop_words_obj.get("init")).run();
+        stop_words_obj.me.put("is_stop_word", (Function<String, Boolean>) w -> ((List<String>)stop_words_obj.get("stop_words")).contains(w));
+        words = (List<String>)data_storage_obj.me.get("words");
+        ((Runnable) stop_words_obj.me.get("init")).run();
         
         HashMap<String, Integer> freqs = new HashMap<String, Integer>();
-        word_freq_obj.put("freqs", freqs);
-        word_freq_obj.put("increment_count", (Runnable) ( () -> {
-			HashMap<String, Integer> freq = ((HashMap<String, Integer>) word_freq_obj.get("freqs"));
-    		final List<String> words2 = (ArrayList<String>) data_storage_obj.get("words");
-    		boolean is_stopword;
+        word_freq_obj.me.put("freqs", freqs);
+        word_freq_obj.me.put("increment_count", (Runnable) ( () -> {
+			HashMap<String, Integer> freq = ((HashMap<String, Integer>) word_freq_obj.me.get("freqs"));
+    		final List<String> words2 = (ArrayList<String>) data_storage_obj.me.get("words");
+    	    boolean is_stopword;
     		for (String word : words2) {
-                is_stopword = ((Function<String, Boolean>) stop_words_obj.get("is_stop_word")).apply(word);
+                is_stopword = ((Function<String, Boolean>) stop_words_obj.me.get("is_stop_word")).apply(word);
                 if((is_stopword==false) && word.length()>1){
         		      if (freq.containsKey(word)){
                 	       freq.put(word, freq.get(word) + 1);
@@ -127,24 +133,24 @@ class Twelve{
         		      }
 			     }
     	     }
-    	     word_freq_obj.put("freqs", freq);
+    	     word_freq_obj.me.put("freqs", freq);
 		}));
-		((Runnable) word_freq_obj.get("increment_count")).run();
+		((Runnable) word_freq_obj.me.get("increment_count")).run();
 		
-		word_freq_obj.put("sorted", (Runnable) ( () -> {
-		    LinkedHashMap<String, Integer> ordered = ((HashMap<String, Integer>)word_freq_obj.get("freqs")).entrySet().stream()
+		word_freq_obj.me.put("sorted", (Runnable) ( () -> {
+		    LinkedHashMap<String, Integer> ordered = ((HashMap<String, Integer>)word_freq_obj.me.get("freqs")).entrySet().stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                         (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-            word_freq_obj.put("freqs", ordered);
+            word_freq_obj.me.put("freqs", ordered);
 		}));
-		((Runnable) word_freq_obj.get("sorted")).run();
+		((Runnable) word_freq_obj.me.get("sorted")).run();
 		
 		word_freq_obj.put("top25", (Runnable) ( () -> {
-		    ((Map<String, Integer>)word_freq_obj.get("freqs")).entrySet().stream().limit(25).forEach(p -> System.out.println(p.getKey() + " - " + p.getValue()));
+		    ((Map<String, Integer>)word_freq_obj.me.get("freqs")).entrySet().stream().limit(25).forEach(p -> System.out.println(p.getKey() + " - " + p.getValue()));
 		
 		}));
-		((Runnable) word_freq_obj.get("top25")).run();
+		((Runnable) word_freq_obj.me.get("top25")).run();
 	}
 
 }
